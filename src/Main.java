7a5aed09e3fa37abca2abb00fecf33838753b374
JyothishMypati03 <jyothish1315@gmail.com>
2026-07-01 11:20:49 +0530

    Jyothish Mypati -- Initial project setup with documentation

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+
+    public static  void main (String[] args){
+
+     System.out.println("Data Structure Problems using Java Generics");
+
+
+    }
+
+}
